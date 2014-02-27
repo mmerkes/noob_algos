d3e1f1effc98eb8b25af825fa59dd31db136b0be
Empty Arsenal <matt.merkes@gmail.com>
2014-02-26 16:13:33 -0800

    Wrote insertion sort and selection sort specs and implementations

--- tests/sorts_spec.js
+++ tests/sorts_spec.js
@@ -5,40 +5,72 @@ var sorts = require('../lib/sorts.js'),
     expect = chai.expect,
     should = chai.should();
 
+describe('Booyah should celebrate success', function() {
+  sorts.celebrate();
+});
+
 // Test the Bubble Sort
-describe('Bubble sorts should take an array and sort it', function() {
+describe('Bubble sort should take an array and sort it', function() {
   it('should properly sort an array', function() {
     expect( sorts.bubbleSort( [ 6,5,4,3,2,1 ] ).join('') ).to.equal('123456');
   });
 });
 
 // Optimized Bubble Sort
-describe('Bubble sorts should take an array and sort it', function() {
+describe('Bubble sort should take an array and sort it', function() {
   it('should properly sort an array', function() {
     expect( sorts.optimizedBubbleSort( [ 6,5,4,3,2,1 ] ).join('') ).to.equal('123456');
   });
 
   it('should be faster than a regular bubble sort', function() {
-    var testArray = [], 
-        clone, start, end, 
-        times = {};
+    var results = sorts.testSpeed( 1000, 5, sorts.bubbleSort, 
+                                  sorts.optimizedBubbleSort );
+    var time = { simple: 0, optimized: 0 };
 
-    for( var i = 10000; i > 0; i-- ) {
-      testArray.push(i);
+    for( var i = 0; i < 5; i++ ) {
+      time.simple += results[0][i];
+      time.optimized += results[1][i];
     }
 
-    clone = testArray.slice();
-    start = new Date().getTime();
-    sorts.bubbleSort( clone );
-    end = new Date().getTime();
-    times.simple = end - start;
-
-    clone = testArray.slice();
-    start = new Date().getTime();
-    sorts.optimizedBubbleSort( clone );
-    end = new Date().getTime();
-    times.optimized = end - start;
-
-    expect( times.simple ).to.be.above( times.optimized );
+    expect( time.simple ).to.be.greaterThan( time.optimized );
   });
 });
+
+// Test the Insertion Sort
+describe('Insertion sort should take an array and sort it', function() {
+  it('should properly sort an array', function() {
+    expect( sorts.insertionSort( [ 6,5,4,3,2,1 ] ).join('') ).to.equal('123456');
+  });
+});
+
+// Optimized Insertion Sort
+describe('Insertion sorts should take an array and sort it', function() {
+  it('should properly sort an array', function() {
+    expect( sorts.optimizedInsertionSort( [ 6,5,4,3,2,1 ] ).join('') ).to.equal('123456');
+  });
+
+  it('should be faster than a regular insertion sort', function() {
+    var results = sorts.testSpeed( 1000, 5, sorts.insertionSort, 
+                                  sorts.optimizedInsertionSort );
+
+    var time = { simple: 0, optimized: 0 };
+
+    for( var i = 0; i < 5; i++ ) {
+      time.simple += results[0][i];
+      time.optimized += results[1][i];
+    }
+
+    expect( time.simple ).to.be.greaterThan( time.optimized );
+  });
+});
+
+// Test the Selection Sort
+describe('Selection sort should take an array and sort it', function() {
+  it('should properly sort an array', function() {
+    expect( sorts.selectionSort( [ 6,5,4,3,2,1 ] ).join('') ).to.equal('123456');
+  });
+});
+
+
+
+
