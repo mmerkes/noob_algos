b102ad194ed5f25c07ee81a322c3765b9c8e7f17
Empty Arsenal <matt.merkes@gmail.com>
2014-03-13 18:09:32 -0700

    Wrote test and implemenations for singly list methods: addFirst, addLast, addAt, removeFirst, removeLast, removeAt

--- tests/structs_spec.js
+++ tests/structs_spec.js
@@ -18,5 +18,44 @@ describe('User should be able to create a linked list and call its methods.', fu
     expect( list.data ).to.equal( 1 );
     expect( list instanceof structs.SinglyLinkedList ).to.be.ok;
   });
+
+  it('should allow you to add a node to the beginning of a list', function() {
+    var list = new structs.SinglyLinkedList( [1,2,3] );
+    list.addFirst(9);
+    expect( list.data ).to.equal(9);
+  });
+
+  it('should allow you to add a node to the end of a list', function() {
+    var list = new structs.SinglyLinkedList( [1,2,3] );
+    list.addLast(9);
+    expect( list.next.next.next.data ).to.equal(9);
+  });
+
+  it('should allow you to add a node at a specified index in a list', function() {
+    var list = new structs.SinglyLinkedList( [1,2,3] );
+    list.addAt( 9, 1 );
+    expect( list.next.data ).to.equal(9);
+  });
+
+  it('should allow you to remove the first node and return the value', function() {
+    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var value = list.removeFirst();
+    expect( list.data ).to.equal(2);
+    expect( value ).to.equal(1);
+  });
+
+  it('should allow you to remove the last node and return the value', function() {
+    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var value = list.removeLast();
+    expect( list.next.next ).to.equal(null);
+    expect( value ).to.equal(3);
+  });
+
+  it('should allow you to remove a node at a particular index and return the value', function() {
+    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var value = list.removeAt(1);
+    expect( list.next.data ).to.equal(3);
+    expect( value ).to.equal(2);
+  });
 });
 
