5fa712691a473a218a8d0f0451b2269eb9c6042e
Empty Arsenal <matt.merkes@gmail.com>
2014-03-15 18:48:58 -0700

    Added spec and implementation for addAt and removeAt

--- tests/structs_spec.js
+++ tests/structs_spec.js
@@ -11,51 +11,105 @@ describe('Booyah should celebrate success', function() {
 
 // Singly linked lists
 describe('User should be able to create a linked list and call its methods.', function() {
-  it('should create a linked list when passed an array', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
+  it('should create an empty linked list', function() {
+    var list = new structs.SinglyLinkedList();
 
     expect( list ).to.be.ok;
-    expect( list.data ).to.equal( 1 );
-    expect( list instanceof structs.SinglyLinkedList ).to.be.ok;
+    expect( list.head ).to.equal( null );
   });
 
+  // addFirst()
   it('should allow you to add a node to the beginning of a list', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var list = new structs.SinglyLinkedList();
     list.addFirst(9);
-    expect( list.data ).to.equal(9);
+    expect( list.head.data ).to.equal(9);
   });
 
-  it('should allow you to add a node to the end of a list', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
-    list.addLast(9);
-    expect( list.next.next.next.data ).to.equal(9);
-  });
-
-  it('should allow you to add a node at a specified index in a list', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
-    list.addAt( 9, 1 );
-    expect( list.next.data ).to.equal(9);
-  });
-
+  // removeFirst()
   it('should allow you to remove the first node and return the value', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var list = new structs.SinglyLinkedList();
+    list.addFirst(9);
+    list.addFirst(8);
     var value = list.removeFirst();
-    expect( list.data ).to.equal(2);
-    expect( value ).to.equal(1);
+    expect( list.head.data ).to.equal(9);
+    expect( value ).to.equal(8);
   });
 
+  // contains()
+  it('should allow you to check if it contains a particular value', function() {
+    var list = new structs.SinglyLinkedList();
+    list.addFirst(9);
+    list.addFirst(8);
+    list.addFirst(7);
+
+    expect( list.contains( 8 ) ).to.be.ok;
+    expect( list.contains( 6 ) ).to.not.be.ok;
+  });
+
+  // getLength()
+  it('should allow you to get the length of a list', function() {
+    var list = new structs.SinglyLinkedList();
+    expect( list.getLength() ).to.equal(0);
+    list.addFirst(9);
+    list.addFirst(8);
+    expect( list.getLength() ).to.equal(2);
+  });
+
+  // isEmpty()
+  it('should allow you to check if the list is empty or not', function() {
+    var list = new structs.SinglyLinkedList();
+    expect( list.isEmpty() ).to.be.ok;
+    list.addFirst(9);
+    expect( list.isEmpty() ).to.not.be.ok;
+  });
+
+  // addLast()
+  it('should allow you to add a node to the end of a list', function() {
+    var list = new structs.SinglyLinkedList();
+    list.addFirst(9);
+    list.addFirst(8);
+    list.addLast(10);
+    expect( list.head.next.next.data ).to.equal(10);
+  });
+
+  // removeLast()
   it('should allow you to remove the last node and return the value', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var list = new structs.SinglyLinkedList();
+    expect( list.removeLast() ).to.equal.null;
+    list.addFirst(9);
+    list.addFirst(8);
     var value = list.removeLast();
-    expect( list.next.next ).to.equal(null);
-    expect( value ).to.equal(3);
+    expect( list.head.next ).to.equal(null);
+    expect( value ).to.equal(9);
   });
 
+  // addAt()
+  it('should allow you to add a node at a specified index in a list', function() {
+    var list = new structs.SinglyLinkedList();
+    list.addFirst(9);
+    list.addFirst(7);
+    list.addAt( 8, 1 );
+    expect( list.head.next.data ).to.equal(8);
+  });
+
+  // removeAt()
   it('should allow you to remove a node at a particular index and return the value', function() {
-    var list = new structs.SinglyLinkedList( [1,2,3] );
+    var list = new structs.SinglyLinkedList();
+    list.addFirst(9);
+    list.addFirst(8);
+    list.addFirst(7);
     var value = list.removeAt(1);
-    expect( list.next.data ).to.equal(3);
-    expect( value ).to.equal(2);
+    expect( list.head.next.data ).to.equal(9);
+    expect( value ).to.equal(8);
+  });
+
+  // clear()
+  it('should allow you to clear the linked list', function() {
+    var list = new structs.SinglyLinkedList();
+    list.addFirst(9);
+    list.addFirst(8);
+    list.clear();
+    expect( list.head ).to.equal(null);
   });
 });
 
